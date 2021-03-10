e437e89cc08ff4844c46bb04509ef97f34f3477d
Ubuntu <ubuntu@ip-172-31-5-74.eu-west-1.compute.internal>
2021-03-10 15:06:47 +0000

    Añadido ficheros terraform

---
+++ ejemplos/ejemplo1/outputs.tf
@@ -0,0 +1,4 @@
+output "ip_contenedor" {
+    value = docker_container.contenedor_nginx
+}
+
---
+++ ejemplos/ejemplo1/variables.tf
@@ -0,0 +1,10 @@
+variable imagen_contenedor {
+    type = string
+    default = "nginx:latest"
+}
+
+variable contenedores {
+    type = list(string)
+    default = []
+}
+
